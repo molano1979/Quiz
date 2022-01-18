4f5434f619e019b48644d8cbb5b0ad798856891a
Diana <dmmolano@yahoo.com>
2022-01-17 22:44:50 -0800

    started working on questions

--- questions.js
+++ questions.js
@@ -1,9 +1,24 @@
 var userName = document.querySelector("#userName");
 var headerText = document.querySelector("#headerText");
 var greetingInput = document.querySelector("#greetingInput");
+var quiz = document.querySelector("#quiz");
+var answerA = document.querySelector("#answerA");
+var answerB = document.querySelector("#answerB");
+var answerC = document.querySelector("#answerC");
+var answerD = document.querySelector("#answerD");
+
+
+
+
+quiz.style.display = "none";
 
 function greeting() {
     headerText.innerHTML = ""
     headerText.innerHTML = "Hello " + userName.value + ", welcome to The Quiz";
     greetingInput.style.display = "none"
+    quiz.style.display = "block"
+}
+
+function aFunction() {
+    answerA.style.color = "red";
 }
