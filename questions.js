d7f85def07d8abdbf3e9d2ab9263f422848d1fb3
Diana <dmmolano@yahoo.com>
2022-01-17 21:56:39 -0800

    styled the greeting bar in JS so that the greeting display disappears when the name is entered

--- questions.js
+++ questions.js
@@ -1,10 +1,9 @@
 var userName = document.querySelector("#userName");
-
-
 var headerText = document.querySelector("#headerText");
-
+var greetingInput = document.querySelector("#greetingInput");
 
 function greeting() {
     headerText.innerHTML = ""
     headerText.innerHTML = "Hello " + userName.value + ", welcome to The Quiz";
+    greetingInput.style.display = "none"
 }
